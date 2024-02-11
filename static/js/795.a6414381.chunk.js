c0e96a33dfec7ee8a2cf67ae54f63ee87c3d7b23
snap515 <77937504+snap515@users.noreply.github.com>
2024-02-11 18:13:35 +0000

    Deploying to gh-pages from @ snap515/goit-react-hw-08-phonebook@040605ad9d308e2cb3d18db6a74c63d9af16860e 🚀

---
+++ static/js/795.a6414381.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[795],{1795:function(e,o,t){t.r(o);t(2791);var n=t(184);o.default=function(){return(0,n.jsx)("h1",{children:"Welcome to Contacts APP"})}}}]);
+//# sourceMappingURL=795.a6414381.chunk.js.map
